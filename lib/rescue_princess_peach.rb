aebce7cc11bc01305a7d7f8fc9c1624c9ada8e7b
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-03-11 01:23:59 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,8 +1,8 @@
-def rescue_princess_peach(phrase)
+def rescue_princess_peach(NameError)
   puts phrase {"Hooray! Mario has rescued Princess Peach."}
 end
 
 ## Comment the below back in to see method scope in action!
 
-# rescue_princess_peach(phrase)
+# rescue_princess_peach(NameError)
 # puts Hooray! Mario has rescued the Princess Peach.
