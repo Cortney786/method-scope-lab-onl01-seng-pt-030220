dc5413e328ae2b1a6b8e2a4f6e2520c7d5ae9ddf
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-03-11 01:34:22 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -4,5 +4,5 @@ end
 
 ## Comment the below back in to see method scope in action!
 
-# rescue_princess_peach (Pricess Peach)
+# rescue_princess_peach(Hooray Mario)
 # puts Hooray! Mario has rescued the Princess Peach.
